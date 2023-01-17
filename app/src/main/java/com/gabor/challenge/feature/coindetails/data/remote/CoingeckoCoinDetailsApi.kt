5f55752e8d5dcdf499931d408fa1701e2f0cdbf1
ayonymus <ayonymus@gmail.com>
2023-01-17 21:59:15 +0100

    Simplify parsed data from api

--- app/src/main/java/com/gabor/challenge/feature/coindetails/data/remote/CoingeckoCoinDetailsApi.kt
+++ app/src/main/java/com/gabor/challenge/feature/coindetails/data/remote/CoingeckoCoinDetailsApi.kt
@@ -17,14 +17,9 @@ data class CoingeckoCoinDetails(
     @SerializedName("id") val id: String,
     @SerializedName("symbol") val symbol: String,
     @SerializedName("name") val name: String,
-    @SerializedName("categories") val categories: ArrayList<String> = arrayListOf(),
-    @SerializedName("public_notice") val publicNotice: String? = null,
-    @SerializedName("additional_notices") val additionalNotices: ArrayList<String> = arrayListOf(),
     @SerializedName("description") val description: Description? = Description(),
     @SerializedName("links") val links: Links? = Links(),
     @SerializedName("image") val image: Image? = Image(),
-    @SerializedName("country_origin") val countryOrigin: String? = null,
-    @SerializedName("genesis_date") val genesisDate: String? = null,
     @SerializedName("sentiment_votes_up_percentage") val sentimentVotesUpPercentage: Double? = null,
     @SerializedName("sentiment_votes_down_percentage") val sentimentVotesDownPercentage: Double? = null,
     @SerializedName("market_cap_rank") val marketCapRank: Int? = null,
@@ -34,7 +29,6 @@ data class CoingeckoCoinDetails(
     @SerializedName("community_score") val communityScore: Double? = null,
     @SerializedName("liquidity_score") val liquidityScore: Double? = null,
     @SerializedName("public_interest_score") val publicInterestScore: Double? = null,
-    @SerializedName("status_updates") val statusUpdates: ArrayList<String> = arrayListOf(),
     @SerializedName("last_updated") val lastUpdated: String? = null
 )
 
